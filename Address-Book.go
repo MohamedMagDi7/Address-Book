c1942ff65de913becd894b7e225e8ed283cc4d06
MohamedMagDi7 <mohamedmega21@gmail.com>
2017-07-29 11:44:07 +0200

    Address Book Last Update...

--- Address-Book.go
+++ Address-Book.go
@@ -18,6 +18,8 @@ import (
 
 type UserSession struct{
 	err string
+	templates *template.Template
+	store *sessions.CookieStore
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 type PhoneNum struct {
@@ -46,42 +48,49 @@ type UserContacts struct {
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-type GlobalData struct {
+type DatabaseSession struct {
  db *gocql.Session
- templates *template.Template
- store *sessions.CookieStore
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-type AppHandler struct {
-	*GlobalData
-	Handle func(  *GlobalData ,http.ResponseWriter , *http.Request )
+type DBHandler struct {
+	*DatabaseSession
+	Handle func(  *DatabaseSession ,http.ResponseWriter , *http.Request )
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (appHandler AppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request ) {
-	appHandler.Handle(appHandler.GlobalData, w, r )
+
+func (dbHandler DBHandler) ServeHTTP(w http.ResponseWriter, r *http.Request ) {
+	dbHandler.Handle(dbHandler.DatabaseSession, w, r )
 	return
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (user * UserContacts) DeleteContact(db *gocql.Session,id string) error{
-	err := db.Query("delete from user_data where username = ? and contact_id = ?",user.UserName , id).Exec()
+type AppHandler struct {
+	Handle func(http.ResponseWriter , *http.Request )
+}
+
+func (appHandler AppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request ) {
+	appHandler.Handle( w, r )
+	return
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+func (user * UserContacts) DeleteContact(databaseSession * DatabaseSession,id string) error{
+	err := databaseSession.db.Query("delete from user_data where username = ? and contact_id = ?",user.UserName , id).Exec()
 	return err
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (user * UserContacts) DeleteContactNumber(db *gocql.Session,id string , contactid string) error{
-	err := db.Query("delete contact_phonenumbers[?] from user_data where username = ? and contact_id = ?",id ,user.UserName , contactid ).Exec()
+func (user * UserContacts) DeleteContactNumber(databaseSession * DatabaseSession,id string , contactid string) error{
+	err := databaseSession.db.Query("delete contact_phonenumbers[?] from user_data where username = ? and contact_id = ?",id ,user.UserName , contactid ).Exec()
 	return err
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (user * UserContacts) GetUserContacts(context *GlobalData) error{
+func (user * UserContacts) GetUserContacts(databaseSession * DatabaseSession) error{
 
 	var newcontact Contact
-	rows := context.db.Query("select contact_id,contact_email,contact_fname,contact_lname,contact_phonenumbers from user_data where username= ?" , user.UserName)
+	rows := databaseSession.db.Query("select contact_id,contact_email,contact_fname,contact_lname,contact_phonenumbers from user_data where username= ?" , user.UserName)
 	scanner :=rows.Iter().Scanner()
 	for scanner.Next(){
 		scanner.Scan(&newcontact.Id , &newcontact.Email, &newcontact.FirstName , &newcontact.LastName , &newcontact.PhoneNumbers)
-		newcontact = StampContactId(newcontact)
+		newcontact.StampContactId()
 		user.Contacts = append(user.Contacts, newcontact)
 	}
 
@@ -89,15 +98,15 @@ func (user * UserContacts) GetUserContacts(context *GlobalData) error{
 	return err
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func UserPage(context *GlobalData,w http.ResponseWriter, r *http.Request){
-	username :=GetUserFromSession(context, r)
+func (context * UserSession) UserPage(databaseSession * DatabaseSession,w http.ResponseWriter, r *http.Request){
+	username :=context.GetUserFromSession( r)
 	if username==""{
 		http.Redirect(w,r,"/home",http.StatusFound)
 		return
 	}
 	user := UserContacts{Contacts:nil,Err:"",UserName:username}
 
-	err :=user.GetUserContacts(context)
+	err :=user.GetUserContacts(databaseSession)
 	if err!=nil{
 		fmt.Println("DB error")
 		http.Error(w,err.Error(),http.StatusInternalServerError)
@@ -112,8 +121,8 @@ func UserPage(context *GlobalData,w http.ResponseWriter, r *http.Request){
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func AddContact(context *GlobalData, w http.ResponseWriter, r *http.Request) {
-	Username :=GetUserFromSession(context,r)
+func (context *UserSession) AddContact(databaseSession * DatabaseSession, w http.ResponseWriter, r *http.Request) {
+	Username :=context.GetUserFromSession(r)
 	user := UserContacts{UserName:Username , Err:""}
 	//Validate Inputs
 
@@ -155,7 +164,7 @@ func AddContact(context *GlobalData, w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println("before insert")
-	c , err := user.InsertNewContact(context , w , r)
+	c , err := user.InsertNewContact(databaseSession , w , r)
 	if err !=nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -167,14 +176,14 @@ func AddContact(context *GlobalData, w http.ResponseWriter, r *http.Request) {
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func Logout(context *GlobalData,w http.ResponseWriter, r *http.Request){
-	SaveUserSession(context ,"", w ,r)
-http.Redirect(w,r,"/home",http.StatusFound)
+func (context *UserSession) Logout( w http.ResponseWriter, r *http.Request){
+	context.SaveUserSession("", w ,r)
+	http.Redirect(w,r,"/home",http.StatusFound)
 	return
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func StampContactId (contact  Contact) Contact{
+func (contact * Contact) StampContactId () {
 	i :=0
 	contact.PhoneNumbersStamped = []PhoneNum{}
 	contactid := contact.Id
@@ -184,10 +193,9 @@ func StampContactId (contact  Contact) Contact{
 		contact.PhoneNumbersStamped = append(contact.PhoneNumbersStamped , PhoneNum{ContactId:contactid , Id:numberid , Phonenumber:phonenumber})
 		i++
 	}
-	return contact
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (user * UserContacts) InsertNewContact(context *GlobalData , w http.ResponseWriter,r *http.Request) (Contact,error){
+func (user * UserContacts) InsertNewContact(databaseSession * DatabaseSession, w http.ResponseWriter,r *http.Request) (Contact,error){
 
 var phonenumbers [] string
 
@@ -206,21 +214,21 @@ c := Contact{
 }
 
 	fmt.Println("before query")
-err := context.db.Query("insert into user_data (username ,contact_id , contact_email , contact_fname , contact_lname , contact_phonenumbers ) values(? , uuid() , ? , ? , ? , ? ) ", user.UserName, r.FormValue("email") , r.FormValue("first-name"), r.FormValue("last-name"), phonenumbers  ).Exec()
+err := databaseSession.db.Query("insert into user_data (username ,contact_id , contact_email , contact_fname , contact_lname , contact_phonenumbers ) values(? , uuid() , ? , ? , ? , ? ) ", user.UserName, r.FormValue("email") , r.FormValue("first-name"), r.FormValue("last-name"), phonenumbers  ).Exec()
 	if err !=nil {
 		fmt.Println(err)
 		return Contact{} , err
 	}
-	c = StampContactId(c)
+	c.StampContactId()
 	fmt.Println(c.PhoneNumbersStamped)
 	user.Contacts = append(user.Contacts, c)
 	return c , nil
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func Delete(context *GlobalData,w http.ResponseWriter, r *http.Request){
-		username :=GetUserFromSession(context,r)
+func(context * UserSession) Delete(databaseSession * DatabaseSession,w http.ResponseWriter, r *http.Request){
+		username :=context.GetUserFromSession(r)
 		user := UserContacts{UserName:username}
-		err := user.DeleteContact(context.db, r.FormValue("id"))
+		err := user.DeleteContact(databaseSession, r.FormValue("id"))
 
 		if err != nil {
 			fmt.Println("DB error")
@@ -230,12 +238,12 @@ func Delete(context *GlobalData,w http.ResponseWriter, r *http.Request){
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func DeleteNum(context *GlobalData,w http.ResponseWriter, r *http.Request){
-	username :=GetUserFromSession(context,r)
+func(context * UserSession) DeleteNum(databaseSession * DatabaseSession,w http.ResponseWriter, r *http.Request){
+	username :=context.GetUserFromSession(r)
 
 		user := UserContacts{UserName:username}
 
-		err := user.DeleteContactNumber(context.db, r.FormValue("id"), r.FormValue("ID"))
+		err := user.DeleteContactNumber(databaseSession, r.FormValue("id"), r.FormValue("ID"))
 
 		if err != nil {
 			fmt.Println("DB error")
@@ -245,9 +253,9 @@ func DeleteNum(context *GlobalData,w http.ResponseWriter, r *http.Request){
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (usersession *UserSession) Check( context *GlobalData,w http.ResponseWriter, r *http.Request) {
+func (context *UserSession) Check(w http.ResponseWriter, r *http.Request) {
 
-	user :=GetUserFromSession(context , r)
+	user :=context.GetUserFromSession(r)
 	if user !="" {
 		http.Redirect(w,r,"/userpage",http.StatusFound)
 		return
@@ -257,35 +265,36 @@ func (usersession *UserSession) Check( context *GlobalData,w http.ResponseWriter
 		}
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func CheckUsernameExists(context *GlobalData , username string) error{
+func (user *UserContacts) CheckUsernameExists(databaseSession * DatabaseSession ) error{
 	var databasePassword string
 
-	err := context.db.Query("SELECT password FROM user_logins WHERE username=?", username).Scan( &databasePassword)
+	err := databaseSession.db.Query("SELECT password FROM user_logins WHERE username=?", user.UserName).Scan( &databasePassword)
 	fmt.Println(err)
 	return err
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (usersession  *UserSession) HomePage(context *GlobalData, w http.ResponseWriter, r *http.Request) {
-	if err := context.templates.ExecuteTemplate(w, "index.html",usersession.err); err != nil {
+func (context  *UserSession) HomePage(w http.ResponseWriter, r *http.Request) {
+	if err := context.templates.ExecuteTemplate(w, "index.html",context.err); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (usersession  *UserSession) SignIn(context *GlobalData, username string, password string , w http.ResponseWriter, r *http.Request ) {
+func (context  *UserSession) SignIn(databaseSession * DatabaseSession, username string, password string , w http.ResponseWriter, r *http.Request ) {
 	// Grab from the database
 	var err error
+	user :=UserContacts{UserName:username}
 	var databasePassword string
-	databasePassword, err = QueryUser(context, username)
+	databasePassword, err = user.QueryUser(databaseSession)
 	if err == gocql.ErrNotFound {
 		//no such user
-		usersession.err="Username doesn't exist"
+		context.err="Username doesn't exist"
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 
 	} else if  err != nil {
 		//Database Error
-		usersession.err="Server Error"
+		context.err="Server Error"
 		http.Error(w,  err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -294,26 +303,26 @@ func (usersession  *UserSession) SignIn(context *GlobalData, username string, pa
 	// If wrong password redirect to the login
 	if  err != nil {
 		//Wrong Password
-		usersession.err="Wrong Password"
+		context.err="Wrong Password"
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	} else {
 		// If the login succeeded
-		SaveUserSession(context,username,w,r)
+		context.SaveUserSession(user.UserName,w,r)
 		http.Redirect(w, r, "/userpage", 301)
 		return
 	}
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (usersession  *UserSession) Register(context *GlobalData,username string ,password string , w http.ResponseWriter, r *http.Request ){
+func (context  *UserSession) Register(databaseSession * DatabaseSession,username string ,password string , w http.ResponseWriter, r *http.Request ){
 
+	user := UserContacts{UserName:username}
 
-
-	err :=CheckUsernameExists(context , username)
+	err :=user.CheckUsernameExists(databaseSession)
 	switch {
 	case err == nil:
 		// Username is not available
-		usersession.err="Please choose a different username"
+		context.err="Please choose a different username"
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	case err == gocql.ErrNotFound:
@@ -321,17 +330,17 @@ func (usersession  *UserSession) Register(context *GlobalData,username string ,p
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 		if err != nil {
 			fmt.Println("Couldn't Incrypt")
-			usersession.err="This Password is Not premitted"
+			context.err="This Password is Not premitted"
 			http.Error(w,err.Error(),http.StatusInternalServerError)
 			return
 		}
 
-		err = InsertUser(context,username,hashedPassword)
+		err = user.InsertUser(databaseSession ,hashedPassword)
 		if err != nil {
 			http.Error(w,err.Error(),http.StatusInternalServerError)
 			return
 		}
-		SaveUserSession(context,username,w,r)
+		context.SaveUserSession(username,w,r)
 		http.Redirect(w, r, "/userpage", http.StatusFound)
 		return
 	case err != nil:
@@ -345,68 +354,63 @@ func (usersession  *UserSession) Register(context *GlobalData,username string ,p
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func (usersession  *UserSession) Login(context *GlobalData, w http.ResponseWriter, r *http.Request){
+func (context  *UserSession) Login(databaseSession * DatabaseSession, w http.ResponseWriter, r *http.Request){
 
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 
 	//Inputs Validation
 	if len(username) > 50 {
-		usersession.err="Username is too long"
+		context.err="Username is too long"
 		http.Redirect(w,r,"/home",301)
 		return
 	}
 	if len(password) > 120 {
 
-		usersession.err="Password is too long"
+		context.err="Password is too long"
 		http.Redirect(w,r,"/home",301)
 		return
 	}
 	if username == "" {
 
-		usersession.err="Please Enter a Username"
+		context.err="Please Enter a Username"
 		http.Redirect(w,r,"/home",301)
 		return
 	}
 	if password == "" {
-		usersession.err="Please Enter a password"
+		context.err="Please Enter a password"
 		http.Redirect(w,r,"/home",301)
 		return
 	}
 	if validateEmail(username) == false {
-		usersession.err="Please Enter a valid Username"
+		context.err="Please Enter a valid Username"
 		http.Redirect(w,r,"/home",301)
 		return
 	}
 
 	if r.FormValue("register")!="" {
-		usersession.Register(context , username ,password ,w,r)
+		context.Register(databaseSession , username ,password ,w,r)
 	}else if r.FormValue("login")!="" {
-		usersession.SignIn(context , username ,password ,w,r)
+		context.SignIn(databaseSession , username ,password ,w,r)
 	}
 
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func Write (w http.ResponseWriter, r *http.Request , next http.HandlerFunc){
-//fmt.Println("I am in middleWare")
-	next(w,r)
-}
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-func QueryUser(context *GlobalData , username string) (string,error){
+func (user * UserContacts) QueryUser(databaseSession * DatabaseSession) (string,error){
 	var databasePassword string
 
-	err := context.db.Query("SELECT password FROM user_logins WHERE username=?", username).Scan( &databasePassword)
+	err := databaseSession.db.Query("SELECT password FROM user_logins WHERE username=?", user.UserName).Scan( &databasePassword)
 	return databasePassword,err
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func SaveUserSession(context *GlobalData, username string , w http.ResponseWriter , r *http.Request){
+func (context * UserSession) SaveUserSession( username string , w http.ResponseWriter , r *http.Request){
 	session , _ := context.store.Get(r,"CurrentSession")
 	session.Values["user"]=username
 	session.Save(r,w)
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func GetUserFromSession(context *GlobalData,r *http.Request) string{
+func (context * UserSession) GetUserFromSession(r *http.Request) string{
 
 	session , _ := context.store.Get(r,"CurrentSession")
 
@@ -419,8 +423,8 @@ func GetUserFromSession(context *GlobalData,r *http.Request) string{
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-func InsertUser(context *GlobalData, username string, hashedPassword []byte) error{
-	err :=context.db.Query("INSERT INTO user_logins(username, password) VALUES(?, ?)",username, hashedPassword).Exec()
+func (user * UserContacts) InsertUser(databaseSession * DatabaseSession, hashedPassword []byte) error{
+	err :=databaseSession.db.Query("INSERT INTO user_logins(username, password) VALUES(?, ?)",user.UserName, hashedPassword).Exec()
 	return err
 
 }
@@ -432,21 +436,23 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
-	Context := &GlobalData{db:db ,
+	databaseSession := &DatabaseSession{db:db}
+
+	context := &UserSession{
 	templates:template.Must(template.ParseFiles("index.html" , "userpage.html")),
-	store :sessions.NewCookieStore([]byte("1819")) }
-	Usersession := &UserSession{err:""}
+	store :sessions.NewCookieStore([]byte("1819")),
+	err:""}
 	mux :=gmux.NewRouter()
 
 
-	mux.Handle("/", AppHandler{Context ,Usersession.Check})
-	mux.Handle("/home", AppHandler{Context , Usersession.HomePage })
-	mux.Handle("/login",AppHandler{Context , Usersession.Login}).Methods("POST")
-	mux.Handle("/userpage", AppHandler{Context , UserPage}).Methods("GET")
-	mux.Handle("/addcontact",AppHandler{Context , AddContact}).Methods("POST")
-	mux.Handle("/logout",AppHandler{Context ,Logout})
-	mux.Handle("/delete", AppHandler{Context ,Delete})
-	mux.Handle("/deletenum", AppHandler{Context ,DeleteNum})
+	mux.Handle("/", AppHandler{context.Check})
+	mux.Handle("/home", AppHandler{ context.HomePage })
+	mux.Handle("/login",DBHandler{databaseSession , context.Login}).Methods("POST")
+	mux.Handle("/userpage", DBHandler{databaseSession , context.UserPage}).Methods("GET")
+	mux.Handle("/addcontact",DBHandler{databaseSession , context.AddContact}).Methods("POST")
+	mux.Handle("/logout",AppHandler{context.Logout})
+	mux.Handle("/delete", DBHandler{databaseSession ,context.Delete})
+	mux.Handle("/deletenum", DBHandler{databaseSession ,context.DeleteNum})
 	n:= negroni.Classic()
 	n.UseHandler(mux)
 	n.Run(":8000")
